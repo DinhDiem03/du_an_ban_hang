068379b0e5c96211c8612abd316ee159a764011b
DinhDiem03 <diemdkph23701@fpt.edu.vn>
2023-04-22 18:46:18 +0700

    first commit

---
+++ src/main/java/com/example/du_an_ban_hang/DuAnBanHangApplication.java
@@ -0,0 +1,13 @@
+package com.example.du_an_ban_hang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuAnBanHangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuAnBanHangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/du_an_ban_hang/DuAnBanHangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.du_an_ban_hang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuAnBanHangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
